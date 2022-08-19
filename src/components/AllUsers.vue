2e2b14fa82210631c49ba3697764ebfbc21828af
panda1 <blueblue0011@gmail.com>
2022-08-18 18:34:20 -0700

    stuck with debugger

---
+++ src/components/AllUsers.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>testing</h1>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+    export default {
+        name: 'all-users',
+        mounted () {
+            axios.request({
+                url: `https://reqres.in/api/users`,
+            }).then((response22) => {
+                for (let i = 0; i< response22.data.length;i++) {
+                document.body.insertAdjacentHTML(`beforeend`, `<h1>${response22[`data`]}</h>`)
+                }
+            }).catch((error) => {
+                console.log(error)
+            });
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
