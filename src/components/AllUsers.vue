310087ef37e2445fc7a625141667b10f716ad904
panda1 <blueblue0011@gmail.com>
2022-09-01 18:54:12 -0700

    rereading and done knowledge check

--- src/components/AllUsers.vue
+++ src/components/AllUsers.vue
@@ -1,24 +1,37 @@
 <template>
     <div>
-        <h1>testing</h1>
+        <h1 ref="display_here">testing</h1>
+        <section v-for="(item, index) in storage" :key="index">
+            <p>{{item.first_name}} {{item.last_name}}</p>
+            <img :src="item.avatar">
+            <p>{{item.email}}</p>
+        </section>
     </div>
 </template>
 
 <script>
 import axios from "axios";
     export default {
-        name: 'all-users',
-        mounted () {
-            axios.request({
-                url: `https://reqres.in/api/users`,
-            }).then((response22) => {
-                for (let i = 0; i< response22.data.length;i++) {
-                document.body.insertAdjacentHTML(`beforeend`, `<h1>${response22[`data`]}</h>`)
-                }
-            }).catch((error) => {
-                console.log(error)
-            });
+        data() {
+            return {
+                storage: []
+            }
         },
+        name: 'all-users',
+        mounted() {
+            axios.request({
+                url: `https://reqres.in/api/users`
+            }).then((success)=>{
+                success
+                this.$refs.display_here.innerHTML = "axios data stored"
+                for(let i = 0; i<success[`data`][`data`].length; i++){
+                this.storage.push(success[`data`][`data`][i])
+                }
+            }).catch((error)=>{
+                error
+                this.$refs.display_here.innerHTML = 'error'
+            })
+        }
     }
 </script>
 
